e6bc1d8c9a1f27ff226451f58d905ab929caf8a9
jignesh02 <jigneshb0208@gmail.com>
2024-06-19 15:44:59 +0000

    Datatypes

---
+++ 01-basics/3rd_datatypes.js
@@ -0,0 +1,34 @@
+// write readble code and code readability should be high
+// use MDN as documentation for javascript
+
+// examples of datatypes
+let name = "Jignesh"; // string
+let age = 20; // number
+let isLoggedIn = true; // boolean
+let state; // undefined
+
+//Datatypes
+
+// Primitive datatypes
+// number => Range of 2 to power of 53
+// bigInt => use for bigger number then 2 to the power of 53. use in stock market website, reddit, facebook
+// string => you can write string in single quote(' ') and double quote (" ") but prefer to use double quote
+// null => standalone value and represntation of empty value you can set value as null. Also null is an object
+// undefined => value is not assign
+// symbol => use as unique charcter
+
+// typeof is use for check datatypes of the variables
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+// Non-Primitive datatypes
+// object
+// Array
+// Function
+// Date
+// RegExp
+// Map
+// Set
+
+// This all are non primitive datatypes

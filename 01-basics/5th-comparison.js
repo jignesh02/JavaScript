4444e96ec6baa866a9685f2811b97cf38825ae78
jignesh02 <jigneshb0208@gmail.com>
2024-06-19 17:50:58 +0000

    comparison facts

---
+++ 01-basics/5th-comparison.js
@@ -0,0 +1,9 @@
+// console.log("2" > 1);
+// console.log("2" === 2); // false because === check strictly including datatypes
+// console.log("02" > 1);
+// javascript convert string into number atomatically
+
+// console.log(null > 0); // false because 0 is not grater than 0
+// console.log(null == 0); // same ||
+// console.log(null >= 0); // comparison convert null into 0 that`s why null >= 0 become true
+// Avoid this type comperison thats make confusion

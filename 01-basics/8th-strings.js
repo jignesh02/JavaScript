b8724c26df8a9877311ccbb3e8bd59f5129483a6
jignesh02 <jigneshb0208@gmail.com>
2024-06-22 16:25:00 +0000

    Strings and its Methods

---
+++ 01-basics/8th-strings.js
@@ -0,0 +1,37 @@
+const myName = "Jignesh";
+let gitRepo = 2;
+
+// console.log("My name is " + myName + "and my github repo is " + gitRepo);
+// do not use this types string concatination use same as below
+
+// console.log(`Hello my name is ${myName} and my github repo is ${gitRepo}`);
+
+const gameName = new String('Jignesh-JB');
+
+// String Methods
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(7));
+// console.log(gameName.indexOf('h'));
+
+// const newString = gameName.substring(1, 5);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 5);
+// console.log(anotherString);
+
+// const newStringOne = "    Jignesh    ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+// console.log(newStringOne.trimStart());
+// console.log(newStringOne.trimEnd());
+
+// const url = "https://jignesh#ahir.com"
+// console.log(url.replace('#', '-'));
+// console.log(url.includes('jhone'));
+
+// console.log(gameName.split('-'));

63df14451137702ea529ef7a5249504ed8d032f0
jignesh02 <jigneshb0208@gmail.com>
2024-06-23 16:58:57 +0000

    Date and Time

---
+++ 01-basics/10th-datetime.js
@@ -0,0 +1,44 @@
+// Dates
+// Date is a object
+
+// let oneDate = new Date();
+// console.log(oneDate.toString());
+// Show date in string formet
+
+// console.log(oneDate.toISOString());
+// show default date
+
+// console.log(oneDate.toJSON());
+// show default date
+
+// console.log(oneDate.toLocaleDateString());
+// show only date like 6/23/2024
+
+// console.log(oneDate.toLocaleString());
+// show date and time like 6/23/2024  1 : 29 :54 PM
+
+// console.log(oneDate.toLocaleTimeString());
+// show only time 1 : 29 :54 PM
+
+// Syntex for declare date
+// let anOtherDate = new Date(2024, 0, 22, 9, 41);
+// console.log(anOtherDate.toLocaleString());
+
+// let twoDate = new Date("02-08-2005");
+// console.log(twoDate.toLocaleString());
+
+// time-stamp in date (use for design quizes and polls)
+
+// let timeStamp = Date.now();
+// console.log(timeStamp);
+// console.log(Math.floor(anOtherDate.getTime()/1000));
+
+let newDate = new Date();
+
+// console.log(newDate.getDate());
+// console.log(newDate.getDay());
+// console.log(newDate.getFullYear());
+// console.log(newDate.getHours());
+// console.log(newDate.getMinutes());
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getTime());

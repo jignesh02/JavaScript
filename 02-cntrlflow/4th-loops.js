f185e2bbc72abb94ea7535e78543590c89fc6532
jignesh02 <jigneshb0208@gmail.com>
2024-08-19 15:48:55 +0000

    Loops

---
+++ 02-cntrlflow/4th-loops.js
@@ -0,0 +1,81 @@
+// For loop
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i == 5) {
+//         console.log("5 is detected");
+//         break
+//     }
+//     console.log(`value of i is ${i}`);
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`outer loop value: ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`inner loop value: ${j} and ${i}`);
+//         console.log(i + ' * ' + j + ' = ' + i * j);
+
+//     }
+// }
+
+
+
+// let myArr = ["flash", "spidermen", "ironmen"];
+// console.log(myArr.length);
+
+
+// for (let index = 0; index <= myArr.length; index++) {
+//     const element = myArr[index];
+//     console.log(element);
+
+
+// }
+
+
+
+// braek and continue
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i == 5) {
+//         console.log("5 is detected");
+//         break
+//     }
+//     console.log(`value of i is ${i}`);
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i == 5) {
+//         console.log("5 is detected");
+//         continue
+//     }
+//     console.log(`value of i is ${i}`);
+// }
+
+
+
+// while loop
+
+// let i = 0;
+// while (i <= 10) {
+//     console.log(`value of i is ${i}`);
+//     i = i + 2;
+// }
+
+// let myArr = ["spidermen", "ironmen", "thor"];
+// let arr = 0;
+
+// while (arr < myArr.length) {
+//     console.log(`value is ${myArr[arr]}`);
+//     arr++;
+// }
+
+
+
+// do while
+
+let score= 1;
+do {
+    console.log(`score is ${score}`);
+    score++;
+    
+} while (score <= 10);
